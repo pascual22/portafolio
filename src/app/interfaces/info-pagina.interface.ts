3042f9ad3b9d0e463f374ef3ae2c939d3e65fb99
Oscar Gonzalez <osmaogonzalez@gmail.com>
2018-11-06 23:31:02 -0500

    Fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nombre?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    equipo_trabajo?: any[];
+}
